09950a6d1ce3e5a6988e5958b74cbbcf9e047059
Adam Coddington <me@adamcoddington.net>
2017-06-07 12:45:05 -0700

    Switch tokenless signature in verification script.

--- manual_verification.py
+++ manual_verification.py
@@ -7,10 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def test():
-    slackpdb.set_trace(
-        u'xoxb-194996974823-UGj992QUwAtJ3hwQSHnx2M6B',
-        channel=u'#что_ты_хочешь'
-    )
+    slackpdb.set_trace(channel=u'#что_ты_хочешь')
 
 
 if __name__ == '__main__':
